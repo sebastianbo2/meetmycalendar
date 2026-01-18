b3c615086f44ad6456c9b18ac3d8872719f83946
nepallium <122187419+nepallium@users.noreply.github.com>
2026-01-18 03:44:54 -0500

    add: getCalendarData endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { getCalendarData } from "./src/getCalendar";
 
 const app = express();
 app.use("cors");
@@ -8,7 +9,13 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.get("/");
+app.post("/getCalendarData", async (req, res) => {
+  const userId = req.body.userId;
+  const googleToken = req.body.googleToken;
+  const response = await getCalendarData(userId, googleToken);
+
+  res.json({ data: response });
+});
 
 app.listen(8000, () => {
   console.log("Server is listening");
